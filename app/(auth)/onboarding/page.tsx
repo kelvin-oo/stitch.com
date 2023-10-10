fa40c61ca69faaac251a61bba9ed526439301caf
obisesan olamilekan <olamilekan.obisesan1@gmail.com>
2023-10-10 20:22:21 +0100

    fixing deployment issues

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -2,26 +2,25 @@ import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-// import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 // import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
   const user = await currentUser();
-  //   if (!user) return null; // to avoid typescript warnings
+    if (!user) return null; // to avoid typescript warnings
 
-  //   const userInfo = await fetchUser(user.id);
-  //   if (userInfo?.onboarded) redirect("/");
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
 
-  const userInfo = {};
 
   const userData = {
-    id: user?.id,
+    id: user.id,
     objectId: userInfo?._id,
-    username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
-    image: userInfo?.image || user?.imageUrl,
-    bio: userInfo?.bio || ""
-  }
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
 
 
   return (
